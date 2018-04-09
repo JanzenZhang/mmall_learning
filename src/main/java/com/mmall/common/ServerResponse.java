b4571a4599b847c50488d03f4123d1f4ab46da30
Zhang Cheng <1401799748@qq.com>
2018-04-10 01:37:58 +0800

    add User Module(register checkValid)

--- src/main/java/com/mmall/common/ServerResponse.java
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -34,7 +34,7 @@ public class ServerResponse<T> implements Serializable {
     }
 
     @JsonIgnore
-    public boolean isSuccess(int status){
+    public boolean isSuccess(){
         return this.status == ResponseCode.SUCCESS.getCode();
     }
 
--- src/main/java/com/mmall/service/IUserService.java
+++ src/main/java/com/mmall/service/IUserService.java
@@ -1,5 +1,12 @@
 package com.mmall.service;
 
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
 public interface IUserService {
-    Object login(String username, String password);
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
 }
